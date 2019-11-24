a4979a50ce6ccbbfa4bb02401fd49b91ba9b671b
hannathkader <hannathabdulkader@gmail.com>
2019-11-24 11:52:30 +0400

    added visibleLinks function
    VisibleLinks function updates page links as per search and shows no result found when matching input is not found.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,18 @@
 /******************************************
-Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-const list=document.querySelectorAll('li.student-item');
-const pageDiv= document.querySelector('div.page');
-const searchDiv=document.createElement('div');
-searchDiv.className='student-search';
+const list = document.querySelectorAll('li.student-item');
+const pageDiv = document.querySelector('div.page');
+const searchDiv = document.createElement('div');
+searchDiv.className = 'student-search';
 pageDiv.firstElementChild.appendChild(searchDiv);
-const searchInput=document.createElement('input');
-searchInput.setAttribute('placeholder','Search for students...');
+const searchInput = document.createElement('input');
+searchInput.setAttribute('placeholder', 'Search for students...');
 searchDiv.appendChild(searchInput);
-const searchButton=document.createElement('button');
-searchButton.textContent='Search';
+const searchButton = document.createElement('button');
+searchButton.textContent = 'Search';
 searchDiv.appendChild(searchButton);
-const names=document.getElementsByTagName('h3');
+const names = document.getElementsByTagName('h3');
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
@@ -44,60 +29,60 @@ const names=document.getElementsByTagName('h3');
        "invoke" the function 
 ***/
 
-const showPage=(list,page)=>{
-   page=parseInt(page)-1;
-   for(let i=0;i<list.length;i++){
-      if(i>=(page*10) &&i<=((10*page)+9)){
-         list[i].style.display='';
+const showPage = (list, page) => {
+   page = parseInt(page) - 1;
+   for (let i = 0; i < list.length; i++) {
+      if (i >= (page * 10) && i <= ((10 * page) + 9)) {
+         list[i].style.display = '';
       }
-      else{
-         list[i].style.display="none";
+      else {
+         list[i].style.display = "none";
       }
-   } 
+   }
 }
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
-const appendPageLinks=list=>{
-   let pageNumber = Math.ceil(list.length/10);
+const appendPageLinks = list => {
+   let pageNumber = Math.ceil(list.length / 10);
    let newDiv = document.createElement("div");
    pageDiv.appendChild(newDiv);
    newDiv.className = 'pagination';
    let ul = document.createElement('ul');
    newDiv.appendChild(ul);
-   for(let i=1;i<=pageNumber;i++){
-    let li=document.createElement('li');
-    let a=document.createElement('a');
-    a.textContent=i;
-    li.appendChild(a);
-    ul.appendChild(li);
+   for (let i = 1; i <= pageNumber; i++) {
+      let li = document.createElement('li');
+      let a = document.createElement('a');
+      a.textContent = i;
+      li.appendChild(a);
+      ul.appendChild(li);
    }
-   let links=document.querySelectorAll('a');
-   links[0].className='active';
-   ul.addEventListener('click',(e)=>{
-   for(let i=0;i<links.length;i++){
-      links[i].classList.remove('active');
-   }
-      if(event.target.tagName==='A'){
-         event.target.className='active';
-         showPage(list,event.target.textContent);
+   let links = document.querySelectorAll('a');
+   links[0].className = 'active';
+   ul.addEventListener('click', (e) => {
+      for (let i = 0; i < links.length; i++) {
+         links[i].classList.remove('active');
+      }
+      if (event.target.tagName === 'A') {
+         event.target.className = 'active';
+         showPage(list, event.target.textContent);
       }
    });
 };
 /*** 
    Create the `SearchName ` to search names in list and display included name.
 ***/
-const searchName=(input,list)=>{
-   const newList=[];
-   for(let i=0;i<list.length;i++){
-      if(names[i].textContent.toLowerCase().includes(input.toLowerCase())){
-         list[i].style.display='';
+const searchName = (input, list) => {
+   const newList = [];
+   for (let i = 0; i < list.length; i++) {
+      if (names[i].textContent.toLowerCase().includes(input.toLowerCase())) {
+         list[i].style.display = '';
          newList.push(list[i]);
       }
-      else{
-         list[i].style.display='none';
+      else {
+         list[i].style.display = 'none';
       }
    }
    return newList;
@@ -105,46 +90,50 @@ const searchName=(input,list)=>{
 /*** 
    Create the `Searchbutton` event to search names based on search button.
 ***/
-searchButton.addEventListener('click',(e)=>{
-   let newList = [ ];
-   e.preventDefault(); 
-   let input=searchInput.value;
-   newList = searchName(input,list);
-   hideButton();
-   appendPageLinks(newList);
-
+searchButton.addEventListener('click', (e) => {
+   e.preventDefault();
+   visibleLinks();
 });
 /*** 
    Create the `SearcInput` event to search names based on keyup event.
 ***/
-searchInput.addEventListener('keyup',(e)=>{
-   let array = [ ];
-   e.preventDefault(); 
-   let input=searchInput.value;
-   if(input!==''){
-   hideButton();
-   array=searchName(input,list);
-   appendPageLinks(array);
-   }
-   else{
-   hideButton();
-   appendPageLinks(list);
-   showPage(list,1); 
-   }
+searchInput.addEventListener('keyup', (e) => {
+   e.preventDefault();
+   visibleLinks();
 });
+
 /*** 
    Created the `hideButton` fuction to hide orginal page numbers.
 ***/
-const hideButton=()=>{
-   let pageNumber=document.querySelectorAll('li a');
-   for(let i=0;i<pageNumber.length;i++){
+const hideButton = () => {
+   let pageNumber = document.querySelectorAll('li a');
+   for (let i = 0; i < pageNumber.length; i++) {
       pageNumber[i].parentNode.removeChild(pageNumber[i]);
    }
 };
+/***created the function `visibleLinks` to show new pagination links and to show name is there or not */
+const visibleLinks = () => {
+   let array = [];
+   let input = searchInput.value;
+   array = searchName(input, list);
+   if (array.length !== 0) {
+      if (input !== '') {
+         hideButton();
+         appendPageLinks(array);
+      }
+      else {
+         hideButton();
+         appendPageLinks(list);
+         showPage(list, 1);
+      }
+   }
+   else {
+      alert('No result found');
+   }
+}
 
 
-
-window.onload=()=>{
+window.onload = () => {
    appendPageLinks(list);
-   showPage(list,1);
+   showPage(list, 1);
 };
